28a64199a18e7d849cb0538788ff2971c035f987
Zonder9162 <zonder9162@gmail.com>
2022-07-05 11:22:09 +0600

    shop

---
+++ parsing_photos/main.py
@@ -0,0 +1,12 @@
+from cgitb import reset
+import requests
+
+url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd2PBOf8cegLps-vR_9UaZi2EG_XgNlfI6qg&usqp=CAU'
+
+res = requests.get(url)
+
+name = "photo1.jpg"
+
+with open(name, "wb") as file:
+    file.write(res.content)
+
